e8428afd9b5c57c4d0b992a4751c598bfaeda690
Xackery <xackery@gmail.com>
2019-05-22 18:08:57 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # eqlog2lua
 Generate quest lua from live eqlog
+
+
+usage: eqlog2lua.exe <file>
+
+e.g. eqloglua.exe eqlog_Player_Server.txt
---
+++ main.go
@@ -0,0 +1,194 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/go-yaml/yaml"
+	"github.com/hpcloud/tail"
+	"github.com/pkg/errors"
+)
+
+var (
+	currentZone   string
+	currentNPC    string
+	youSay        string
+	lastEventTime time.Time
+	dialogs       map[string]*QuestDialog
+)
+
+// QuestDialog is every NPC dialog
+type QuestDialog struct {
+	NPCName      string            `yaml:"npcname"`
+	CurrentZone  string            `yaml:"currentzone"`
+	Conversation map[string]string `yaml:"conversation"`
+}
+
+func main() {
+	dialogs = make(map[string]*QuestDialog)
+	err := run()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	os.Exit(0)
+}
+
+func run() (err error) {
+	if len(os.Args) < 2 {
+		err = fmt.Errorf("usage: eqlog2lua <file>")
+		return
+	}
+
+	var d []byte
+	//load quests file, if exists
+	fi, err := os.Stat("quests.yml")
+	if err == nil {
+		d, err = ioutil.ReadFile("quests.yml")
+		if err != nil {
+			err = errors.Wrap(err, "failed to read quests.yml")
+			return
+		}
+		err = yaml.Unmarshal(d, dialogs)
+		if err != nil {
+			err = errors.Wrap(err, "failed to parse quests.yml")
+			return
+		}
+	}
+
+	filePath := os.Args[1]
+	playerName := ""
+
+	fi, err = os.Stat(filePath)
+	if err != nil {
+		return
+	}
+
+	if !strings.Contains(fi.Name(), ".txt") {
+		err = fmt.Errorf("file %s must contain .txt ending", fi.Name())
+		return
+	}
+
+	if !strings.Contains(fi.Name(), "eqlog_") {
+		err = fmt.Errorf("file %s must contain eqlog_ file prefix", fi.Name())
+		return
+	}
+
+	playerName = fi.Name()[strings.Index(fi.Name(), "eqlog_")+6:]
+	if !strings.Contains(playerName, "_") {
+		err = fmt.Errorf("file %s must contain a eqlog_name_ pattern prefix", fi.Name())
+		return
+	}
+	playerName = playerName[0:strings.Index(playerName, "_")]
+	fmt.Println("parsing", filePath, "with player", playerName)
+
+	cfg := tail.Config{
+		Follow:    true,
+		MustExist: true,
+		Poll:      true,
+		Location: &tail.SeekInfo{
+			Offset: fi.Size(),
+		},
+	}
+	t, err := tail.TailFile(filePath, cfg)
+	if err != nil {
+		err = errors.Wrap(err, "failed to read log")
+		return
+	}
+	for line := range t.Lines {
+		err = doParse(line.Text)
+		if err != nil {
+			fmt.Println("failed to parse line (", line.Text, ") error:", err)
+			err = nil
+		}
+	}
+	return
+}
+
+func doParse(line string) (err error) {
+	//rawLine := line
+	tsIndex := strings.Index(line, "]")
+	if tsIndex < 0 {
+		err = fmt.Errorf("no timestamp ] found")
+		return
+	}
+	//timestamp := line[0:tsIndex]
+	line = line[tsIndex+2:]
+	fmt.Println(line)
+	if strings.Index(line, "You have entered") == 0 {
+		currentZone = line[strings.Index(line, "You have entered")+16:]
+		echoPrint("current zone changed")
+		return
+	}
+	if strings.Index(line, "You say, '") == 0 {
+		youSay = line[strings.Index(line, "You say, '")+10 : len(line)-2]
+		if strings.Index(youSay, "Hail, ") == 0 {
+			youSay = "hail"
+		}
+		echoPrint("You said: " + youSay)
+		return
+	}
+	if len(youSay) > 0 && strings.Index(line, "says, '") > 0 {
+		npcName := line[0 : strings.Index(line, "says, ")-1]
+		currentNPC = npcName
+		qd, ok := dialogs[npcName]
+		if !ok {
+			qd = &QuestDialog{
+				NPCName:      npcName,
+				Conversation: make(map[string]string),
+				CurrentZone:  currentZone,
+			}
+		}
+		qd.Conversation[youSay] = line[strings.Index(line, "says, '")+7 : len(line)-3]
+
+		echoPrint(qd.NPCName + " said: " + qd.Conversation[youSay])
+		dialogs[qd.NPCName] = qd
+		youSay = ""
+		err = saveYaml()
+		return
+	}
+	if len(youSay) > 0 && strings.Index(line, "You have been assigned the task") == 0 {
+		if len(currentNPC) == 0 {
+			err = fmt.Errorf("no npc set but assigned task")
+			return
+		}
+		qd, ok := dialogs[currentNPC]
+		if !ok {
+			qd = &QuestDialog{
+				NPCName:      currentNPC,
+				Conversation: make(map[string]string),
+				CurrentZone:  currentZone,
+			}
+		}
+		qd.Conversation[youSay] = line[0 : len(line)-1]
+
+		echoPrint(qd.NPCName + " gave you a task! " + qd.Conversation[youSay])
+		dialogs[qd.NPCName] = qd
+		youSay = ""
+		err = saveYaml()
+		return
+	}
+
+	return
+}
+
+func echoPrint(message string) {
+	fmt.Printf("Z:%s|NPC:%s > %s\n", currentZone, currentNPC, message)
+}
+
+func saveYaml() (err error) {
+	d, err := yaml.Marshal(&dialogs)
+	if err != nil {
+		err = errors.Wrap(err, "failed to marshal quest dialog")
+		return
+	}
+	err = ioutil.WriteFile("quests.yml", d, 0744)
+	if err != nil {
+		err = errors.Wrap(err, "Failed to write quests file")
+		return
+	}
+	return
+}
